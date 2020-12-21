7c61dfc35fe555377596f39dc42d200579374244
thach <vungocthach1999@gmail.com>
2020-12-21 08:13:10 +0700

    hoa don

---
+++ blog/resources/views/hoadon.blade.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bán hàng</title>
+    <link rel="stylesheet" href="css/all.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="sidebar-container">
+        <div class="sidebar-logo" style="font-family: 'Times New Roman', Times, serif;">
+            MILKTEA TSU
+        </div>
+        <ul class="sidebar-navigation">
+            <li class="header" style="font-family: 'Times New Roman', Times, serif;">BÁN HÀNG</li>
+
+            <li>
+                <a href="hoadon.html" style="text-decoration: none;font-family: 'Times New Roman', Times, serif">
+                    <i class="fa fa-tachometer" aria-hidden="true"></i>HÓA ĐƠN
+                </a>
+            </li>
+            <li>
+                <a href="lichsugd.html" style="text-decoration: none;font-family: 'Times New Roman', Times, serif">
+                    <i class="fa fa-tachometer" aria-hidden="true"></i>LỊCH SỬ GIAO DỊCH
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="content-container">
+
+        <div class="container-fluid">
+
+            <div class="container-sp">
+                <div class="navbar">
+                    <h2 class="title" style="margin-left: 20%;">DANH SÁCH SẢN PHẨM</h2>
+                </div>
+                <div class="active-cyan-4 mb-4" style="margin: 2%;">
+                    <input class="form-control" type="text" placeholder="Tìm kiếm..." aria-label="Search">
+                </div>
+                <div>
+                    <h3 class="h3" style="font-family: 'Times New Roman', Times, serif;">TRÀ SỮA</h3>
+                </div>
+                <div>
+                    <div class="row" style="margin: 2%">
+                        <div class="col-3">
+                            <div class="card">
+                                <img src="images/trasua1.PNG" alt="Avatar" style="width:100%">
+                                <div class="container-info">
+                                    <h4><b>Trà Sữa Truyền Thống</b></h4>
+                                    <p>23.000đ</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" col-3">
+                            <div class="card">
+                                <img src="images/trasua2.PNG" alt="Avatar" style="width:100%">
+                                <div class="container-info">
+                                    <h4><b>Trà Sữa Socola</b></h4>
+                                    <p>25.000đ</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" col-3">
+                            <div class="card">
+                                <img src="images/trasua3.PNG" alt="Avatar" style="width:100%">
+                                <div class="container-info">
+                                    <h4><b>Trà Sữa Khoai Môn</b></h4>
+                                    <p>25.000đ</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="card">
+                                <img src="images/trasua4.PNG" alt="Avatar" style="width:100%">
+                                <div class="container-info">
+                                    <h4><b>Trà Sữa Trái Cây</b></h4>
+                                    <p>25.000đ</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row" style="margin: 2%">
+
+                        <div class=" col-3">
+                            <div class="card">
+                                <img src="images/trasua5.PNG" alt="Avatar" style="width:100%">
+                                <div class="container-info">
+                                    <h4><b>Trà Sữa Olong</b></h4>
+                                    <p>23.000đ</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" col-3">
+                            <div class="card">
+                                <img src="images/trasua6.PNG" alt="Avatar" style="width:100%">
+                                <div class="container-info">
+                                    <h4><b>Trà Sữa Thái</b></h4>
+                                    <p>25.000đ</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="card">
+                                <img src="images/trasua7.PNG" alt="Avatar" style="width:100%">
+                                <div class="container-info">
+                                    <h4><b>Trà Sữa Hoa Lài</b></h4>
+                                    <p>23.000đ</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" col-3">
+                            <div class="card">
+                                <img src="images/trasua8.PNG" alt="Avatar" style="width:100%">
+                                <div class="container-info">
+                                    <h4><b>Trà Sữa MatCha</b></h4>
+                                    <p>25.000đ</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h3 class="h3" style="font-family: 'Times New Roman', Times, serif;">TRÀ </h3>
+                    </div>
+                    <div class="row" style="margin: 2%">
+                        <div class=" col-3">
+                            <div class="card">
+                                <img src="images/trasua9.PNG" alt="Avatar" style="width:100%">
+                                <div class="container-info">
+                                    <table>
+                                        <tr>
+                                            <h4><b>Trà Sen Creama</b></h4>
+                                        </tr>
+                                        <tr>
+                                            <p>25.000đ</p>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" col-3">
+                            <div class="card">
+                                <img src="images/trasua10.PNG" alt="Avatar" style="width:100%">
+                                <div class="container-info">
+                                    <tr>
+                                        <h4><b>Trà Olong Vải</b></h4>
+                                    </tr>
+                                    <tr>
+                                        <p>28.000đ</p>
+                                    </tr>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" col-3">
+                            <div class="card">
+                                <img src="images/trasua11.PNG" alt="Avatar" style="width:100%">
+                                <div class="container-info">
+                                    <tr>
+                                        <h4><b>Trà Đào</b></h4>
+                                    </tr>
+                                    <tr>
+                                        <p>23.000đ</p>
+                                    </tr>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" col-3">
+                            <div class="card">
+                                <img src="images/trasua12.PNG" alt="Avatar" style="width:100%">
+                                <div class="container-info">
+                                    <tr>
+                                        <h4><b>Trà Xoài</b></h4>
+                                    </tr>
+                                    <tr>
+                                        <p>25.000đ</p>
+                                    </tr>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="container-hd">
+                <div>
+                    <h2 class="title">HÓA ĐƠN</h2>
+                </div>
+
+                <div class="container-bhd" style="margin-top: 8%; border-radius: 5%    ;">
+                    <table>
+
+                    </table>
+                </div>
+                <div class="row" style="margin: 2%;">
+                    <button style=" margin-left: 60%;" class="btn btn-primary col-4">Thanh toán </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
